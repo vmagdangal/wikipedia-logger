23c46d5e2d4021855b6b4f3cdd25f8746f489a29
vmagdangal <vmagdangal257@gmail.com>
2025-07-30 15:33:52 -0700

    feat: sort/filter + library format
    - library can now sort and filter
    - listings are made shorter and more concise

--- pages/2_library.py
+++ pages/2_library.py
@@ -3,9 +3,13 @@ import sqlite3
 import pandas as pd # type: ignore
 import wikipedia #type: ignore
 
-st.set_page_config(page_title="Article Library", page_icon="📊")
-st.markdown(f"**Articles**")
-max_summary_length = 350
+st.set_page_config(page_title="Article Library", page_icon="📊", layout="wide")
+MAX_SUMMARY_LENGTH = 350
+WIDTH_SORT = [4, 2, 1]
+WIDTH_FILTER = [4, 1, 1]
+WIDTH_ARTICLE_BADGE = [4, 4, 1]
+WIDTH_EDIT_ENTRY = [4, 1]
+WIDTH_2COL_EVEN = [1, 1]
 
 class Category:
   def __init__(self, category_id, category_name):
@@ -150,31 +154,75 @@ try:
         GROUP BY a.article_id, a.title, a.was_read, a.date_added, a.link
     """, sqliteConnection)
 
-    for index, row in df.iterrows():
-        page = wikipedia.WikipediaPage(title=row.title)
+    category_map = {c.category_name: c for c in grab_categories()}
+    
+    st.title("Library")
 
-        with st.container(border=True):
-            title, link = st.columns([5, 1], vertical_alignment="center")
-            title.markdown(f"**{row.title}**")
-            link.link_button("Visit 🔗", row.link)
+    with st.expander("Sort & Filter", icon=":material/sort:"):
+        if "df_using" not in st.session_state:
+            st.session_state.df_using = df.copy()
 
-            category_markdown = ""
-            lib_categories = row.categories.split("|")
-            lib_categories.sort()
-            for category in lib_categories:
-                category_markdown += f":blue-background[{category}] "
-            st.markdown(category_markdown)
-            
-            if(len(page.summary) > max_summary_length):
-                st.write(page.summary[:max_summary_length - 3] + "...")
-            else:
-                st.write(page.summary)
+        SORT_COLUMNS = {
+            "Title": "title",
+            "Date": "date_added"
+        }
+        
+        sort_options, sort_dir_options, sort_confirm = st.columns(WIDTH_SORT, vertical_alignment="bottom")
+        sort_selection = sort_options.selectbox(
+            "Sort By",
+            ("Title", "Date"),
+        )
+        sort_direction = sort_dir_options.radio(
+            "Order",
+            ["Ascending", "Descending"],
+            key="sort_direction"
+        )
+        if sort_confirm.button("Sort", use_container_width=True, key=f"sort_confirm"):
+            st.session_state.df_using = (df
+                .copy()
+                .sort_values(by=SORT_COLUMNS[sort_selection], ascending=(sort_direction == "Ascending"))
+            )
 
-            date_badge, empty, read_badge= st.columns([2, 4, 1], vertical_alignment="center")
-            date_badge.badge(row.date_added, icon=":material/calendar_today:", color="blue")
+        st.divider()
+        filter_options, filter_confirm, reset_filter = st.columns(WIDTH_FILTER, vertical_alignment="bottom")
+        filter_selection = filter_options.selectbox(
+            "Filter Category",
+            list(category_map.keys()),
+        )
+        if filter_confirm.button("Filter", use_container_width=True, key=f"filter_confirm"):
+            st.session_state.df_using = df.copy()[df["categories"].str.contains(f"{filter_selection}", na=False)]
+        if reset_filter.button("Reset", use_container_width=True, key=f"filter_reset"):
+            st.session_state.df_using = df.copy()
 
-            with st.expander("Edit Entry"):
-                mark_read, delete = st.columns([1, 1], vertical_alignment="center")
+        df_using = st.session_state.df_using
+
+    st.markdown(f"**Articles** (Size: {len(df_using)})")
+    with st.container(border=True):
+        for index, row in df_using.iterrows():
+            page = wikipedia.WikipediaPage(title=row.title)
+            with st.expander(f"{row.title}"):
+
+                st.divider()
+                st.subheader(f"{row.title}")
+                category_markdown = ""
+                lib_categories = row.categories.split("|")
+                lib_categories.sort()
+                for category in lib_categories:
+                    category_markdown += f":blue-background[{category}] "
+                st.markdown(category_markdown)
+                
+                if(len(page.summary) > MAX_SUMMARY_LENGTH):
+                    st.write(page.summary[:MAX_SUMMARY_LENGTH - 3] + "...")
+                else:
+                    st.write(page.summary)
+
+                date_badge, link, read_badge= st.columns(WIDTH_ARTICLE_BADGE, vertical_alignment="center")
+                date_badge.badge(row.date_added, icon=":material/calendar_today:", color="blue")
+                link.link_button("Visit 🔗", row.link)
+
+                st.divider()
+                st.markdown("**Edit Entry**")
+                mark_read, delete = st.columns(WIDTH_2COL_EVEN, vertical_alignment="center")
                 if(row.was_read == "True"):
                     read_badge.badge("Read", icon=":material/check:", color="green")
                     if mark_read.button("Mark as Want to Read", icon="⌛️", use_container_width=True, key=f"wantread_{row.article_id}"):
@@ -186,13 +234,12 @@ try:
                 if delete.button("Delete from Library", icon="🗑️", use_container_width=True, key=f"delete_{row.article_id}"):
                     delete_article(row.article_id)
 
-                date_options, date_save = st.columns([4, 1], vertical_alignment="bottom")
+                date_options, date_save = st.columns(WIDTH_EDIT_ENTRY, vertical_alignment="bottom")
                 new_date = date_options.date_input("Date Read", row.date_added)
                 if date_save.button("Update", icon="🔄", use_container_width=True, key=f"date_{row.article_id}"):
                     update_date(row.article_id, new_date)
 
-                category_map = {c.category_name: c for c in grab_categories()}
-                category_options, category_save = st.columns([4, 1], vertical_alignment="bottom")
+                category_options, category_save = st.columns(WIDTH_EDIT_ENTRY, vertical_alignment="bottom")
                 new_categories = category_options.multiselect("Select Categories", 
                                         list(category_map.keys()), 
                                         accept_new_options=False,
